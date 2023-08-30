d5148d3afce69e659703f4e9bacec405be82605a
Dixie Dean <d.w.sokolov26@gmail.com>
2023-08-30 12:53:25 +0300

    Initial Commit

---
+++ src/main/java/com/example/homeworkspringboot/HomeworkSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkspringboot/HomeworkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
